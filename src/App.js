4e01c71c51081dfa43fbf275cd630ec28e807e0e
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-25 15:48:15 +0900

    프로젝트 초기화

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-import { useState } from "react";
-
 export default function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
-
-  const addTodo = (e) => {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  };
-
-  return (
-    <div>
-      {todos.map((item, index) => (
-        <div key={index}>
-          <input type="checkbox"></input>
-          <label>{item}</label>
-        </div>
-      ))}
-
-      <form>
-        <input value={todo} onChange={(e) => setTodo(e.target.value)}></input>
-        <button onClick={addTodo}>추가</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
